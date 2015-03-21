a3de8c6a489db66e1df38c2871a84b68f0515287
yogeshmadaan100 <yogeshmadaan100@gmail.com>
2015-03-21 11:51:31 +0530

    Port Commit

---
+++ obj/local/armeabi-v7a/objs/mupdfcore/__/__/__/source/fitz/getopt.o.d
@@ -0,0 +1,59 @@
+obj/local/armeabi-v7a/objs/mupdfcore/__/__/__/source/fitz/getopt.o: \
+ jni/../../../source/fitz/getopt.c \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h:
---
+++ obj/local/armeabi-v7a/objs/mupdfthirdparty/__/__/__/thirdparty/jpeg/jerror.o.d
@@ -0,0 +1,88 @@
+obj/local/armeabi-v7a/objs/mupdfthirdparty/__/__/__/thirdparty/jpeg/jerror.o: \
+ jni/../../../thirdparty/jpeg/jerror.c \
+ jni/../../../thirdparty/jpeg/jinclude.h ../../scripts/jpeg/jconfig.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/strings.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/memory.h \
+ /home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdio.h \
+ jni/../../../thirdparty/jpeg/jpeglib.h \
+ jni/../../../thirdparty/jpeg/jmorecfg.h \
+ jni/../../../thirdparty/jpeg/jversion.h \
+ jni/../../../thirdparty/jpeg/jerror.h
+
+jni/../../../thirdparty/jpeg/jinclude.h:
+
+../../scripts/jpeg/jconfig.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/string.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/home/yogesh/Downloads/android-ndk-r10d/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+jni/../../../thirdparty/jpeg/jpeglib.h:
+
+jni/../../../thirdparty/jpeg/jmorecfg.h:
+
+jni/../../../thirdparty/jpeg/jversion.h:
+
+jni/../../../thirdparty/jpeg/jerror.h:
